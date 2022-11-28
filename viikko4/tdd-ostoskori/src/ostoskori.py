cee0db5b4e079cc4128bc08c982197c3c16cba59
Ville Jouppi <jope@jope.fi>
2022-11-28 20:22:12 +0200

    testi 2. Yhden tuotteen lisäämisen jälkeen ostoskorissa on 1 tavara.

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -3,11 +3,14 @@ from ostos import Ostos
 
 class Ostoskori:
     def __init__(self):
-        pass
+        self.ostokset = {}
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
 
     def tavaroita_korissa(self):
-        return 0
+        lkm = 0
+        for ostos in self.ostokset.keys():
+            lkm += self.ostokset[ostos].lukumaara()
+        return lkm
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
@@ -17,8 +20,11 @@ class Ostoskori:
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
-        # lisää tuotteen
-        pass
+        nimi = lisattava.nimi()
+        if nimi in self.ostokset:
+            self.ostokset[nimi].muuta_lukumaaraa(1)
+        else:
+            self.ostokset[nimi] = Ostos(lisattava)
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
