450c1f9dee87274dac434d71c97c08911dce7e65
Ville Jouppi <jope@jope.fi>
2022-11-28 20:48:00 +0200

    testi 8. Yhden tuotteen lisäämisen jälkeen ostoskori sisältää yhden ostoksen

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -3,13 +3,13 @@ from ostos import Ostos
 
 class Ostoskori:
     def __init__(self):
-        self.ostokset = {}
+        self.sisalto = {}
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
 
     def tavaroita_korissa(self):
         lkm = 0
-        for ostos in self.ostokset.keys():
-            lkm += self.ostokset[ostos].lukumaara()
+        for ostos in self.sisalto.keys():
+            lkm += self.sisalto[ostos].lukumaara()
         return lkm
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
@@ -17,17 +17,17 @@ class Ostoskori:
 
     def hinta(self):
         summa = 0
-        for ostos in self.ostokset.keys():
-            summa += self.ostokset[ostos].hinta()
+        for ostos in self.sisalto.keys():
+            summa += self.sisalto[ostos].hinta()
         return summa
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
         nimi = lisattava.nimi()
-        if nimi in self.ostokset:
-            self.ostokset[nimi].muuta_lukumaaraa(1)
+        if nimi in self.sisalto:
+            self.sisalto[nimi].muuta_lukumaaraa(1)
         else:
-            self.ostokset[nimi] = Ostos(lisattava)
+            self.sisalto[nimi] = Ostos(lisattava)
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
@@ -38,6 +38,6 @@ class Ostoskori:
         # tyhjentää ostoskorin
 
     def ostokset(self):
-        pass
+        return list(self.sisalto.values())
         # palauttaa listan jossa on korissa olevat ostos-oliot
         # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -45,3 +45,9 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.hinta(), 6)
+
+    def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_ostosolio(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        ostokset = self.kori.ostokset()
+        self.assertEqual(len(ostokset), 1)
