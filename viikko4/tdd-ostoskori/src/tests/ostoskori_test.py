24c34e7ad19a2312ce87c74f8a9d6876d6fa5a9d
Ville Jouppi <jope@jope.fi>
2022-11-28 20:55:13 +0200

    testi 10. Kahden eri tuotteen lisäämisen jälkeen ostoskori sisältää kaksi ostosta

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -58,3 +58,11 @@ class TestOstoskori(unittest.TestCase):
         ostos = self.kori.ostokset()[0]
         self.assertEqual(ostos.tuote, maito)
         self.assertEqual(ostos.lukumaara(), 1)
+
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_ostoskorissa_on_2_ostosta(self):
+        maito = Tuote("Maito", 3)
+        kerma = Tuote("Kerma", 4)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(kerma)
+        ostokset = self.kori.ostokset()
+        self.assertEqual(len(ostokset), 2)
