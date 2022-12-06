5090f2049de96dcaf159869dc94c8560471d0ae9
Ville Jouppi <jope@jope.fi>
2022-12-06 20:51:51 +0200

    viikko5 tehtävä3

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -2,56 +2,49 @@ class TennisGame:
     def __init__(self, player1_name, player2_name):
         self.player1_name = player1_name
         self.player2_name = player2_name
-        self.m_score1 = 0
-        self.m_score2 = 0
+        self.player1_points = 0
+        self.player2_points = 0
 
     def won_point(self, player_name):
-        if player_name == "player1":
-            self.m_score1 = self.m_score1 + 1
+        if player_name == self.player1_name:
+            self.player1_points = self.player1_points + 1
+        if player_name == self.player2_name:
+            self.player2_points = self.player2_points + 1
+
+    def endgame_points_to_text(self, points1, points2):
+        points_difference = points1 - points2
+
+        if points_difference == 1:
+            return("Advantage " + self.player1_name)
+        elif points_difference == -1:
+            return("Advantage " + self.player2_name)
+        elif points_difference >= 2:
+            return("Win for " + self.player1_name)
         else:
-            self.m_score2 = self.m_score2 + 1
+            return("Win for " + self.player2_name)
+
+    def points_to_text(self, points):
+        if points == 0:
+            return("Love")
+        elif points == 1:
+            return("Fifteen")
+        elif points == 2:
+            return("Thirty")
+        elif points == 3:
+            return("Forty")
+
+    def tied_points_to_text(self, points):
+        if points >= 4:
+            return("Deuce")
+        else:
+            return(self.points_to_text(points) + "-All")
 
     def get_score(self):
-        score = ""
-        temp_score = 0
-
-        if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
-                score = "Love-All"
-            elif self.m_score1 == 1:
-                score = "Fifteen-All"
-            elif self.m_score1 == 2:
-                score = "Thirty-All"
-            elif self.m_score1 == 3:
-                score = "Forty-All"
-            else:
-                score = "Deuce"
-        elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
-
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
-            else:
-                score = "Win for player2"
+        if self.player1_points == self.player2_points:
+            return(self.tied_points_to_text(self.player1_points))
+        elif self.player1_points >= 4 or self.player2_points >= 4:
+            return(self.endgame_points_to_text(self.player1_points, self.player2_points))
         else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.m_score1
-                else:
-                    score = score + "-"
-                    temp_score = self.m_score2
-
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
-
-        return score
+            player1_score = self.points_to_text(self.player1_points)
+            player2_score = self.points_to_text(self.player2_points)
+            return(player1_score + "-" + player2_score)
